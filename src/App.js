5a8b065c939c9675a91086a3b0a9d0839bce124f
Tulio <tulio.mminini@gmail.com>
2023-10-30 16:54:55 +0000

    :lipstick: hero compoment for enter page started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import style from './App.module.css';
 
 import { EnterPage } from './pages/EnterPage/EnterPage';
 import { HomePage } from './pages/HomePage/HomePage';
 
 function App() {
   return (
-    <div className="App">
+    <div className={style.app}>
       <Routes>
         <Route path="/" element={<EnterPage />} />
         <Route path="/home" element={<HomePage />} />
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,34 @@
 import style from './Hero.module.css';
+import { NavLink } from 'react-router-dom';
+import heroImg from './../../assets/images/nainoa-shizuru-image.jpg';
 
 export function Hero() {
   return (
     <main className={style.enterContainer}>
-      <h1>Enter Page</h1>
+      <section className={style.leftContainer}>
+        <footer>
+          photography by
+          <a
+            href="https://www.nainoashizuru.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Nainoa Shizuru
+          </a>
+        </footer>
+        <img
+          src={heroImg}
+          alt="a crowd in a concert, during the night, with the main colors being orange and black"
+        />
+      </section>
+      <section className={style.rightContainer}>
+        <h1>concerts nostalgia</h1>
+        <h2>my live concerts journey, revisited</h2>
+        <NavLink to="/home">enter</NavLink>
+        <footer>
+          &copy; 2023 Created, designed and built by by Tulio Minini
+        </footer>
+      </section>
     </main>
   );
 }
