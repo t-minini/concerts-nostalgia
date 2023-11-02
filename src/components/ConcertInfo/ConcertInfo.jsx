7d4c03230c9ef669aecca13f84cbdd9fff99bab9
Tulio <tulio.mminini@gmail.com>
2023-11-02 21:26:00 +0000

    :lipstick: ConcertInfo component started

--- src/components/ConcertInfo/ConcertInfo.jsx
+++ src/components/ConcertInfo/ConcertInfo.jsx
@@ -3,7 +3,7 @@ import style from './ConcertInfo.module.css';
 export function ConcertInfo() {
   return (
     <main className={style.concertInfo}>
-      <h1>Concert Info</h1>
+      <section className={style.infoContainer}></section>
     </main>
   );
 }
--- src/components/ConcertsList/ConcertsList.jsx
+++ src/components/ConcertsList/ConcertsList.jsx
@@ -2,13 +2,17 @@ import style from './ConcertsList.module.css';
 import robyn from './../../assets/images/robyn-ticket.png';
 import halsey from './../../assets/images/halsey-ticket.png';
 import coldplay from './../../assets/images/coldplay-ticket.png';
+import { ConcertInfo } from '../ConcertInfo/ConcertInfo';
 
 export function ConcertsList() {
   return (
     <main id="tickets" className={style.concertsList}>
-      <img src={halsey} alt="concert ticket" />
-      <img src={coldplay} alt="concert ticket" />
-      <img src={robyn} alt="concert ticket" />
+      <div className={style.ticketsContainer}>
+        <img src={halsey} alt="concert ticket" />
+        <img src={coldplay} alt="concert ticket" />
+        <img src={robyn} alt="concert ticket" />
+      </div>
+      <ConcertInfo />
     </main>
   );
 }
