d5d635b5f35602367df6c68bfa31ae8ded2af7fc
Tulio <tulio.mminini@gmail.com>
2023-11-02 20:25:39 +0000

    :lipstick: small bug on footer copyright fixed, ConcertsList component started

--- src/components/ConcertsList/ConcertsList.jsx
+++ src/components/ConcertsList/ConcertsList.jsx
@@ -1,5 +1,14 @@
 import style from './ConcertsList.module.css';
+import halsey from './../../assets/images/halsey-ticket.png';
+import coldplay from './../../assets/images/coldplay-ticket.png';
+import robyn from './../../assets/images/robyn-ticket.png';
 
 export function ConcertsList() {
-  return <main className={style.concertsList}></main>;
+  return (
+    <main className={style.concertsList}>
+      <img src={halsey} alt="concert ticket" />
+      <img src={coldplay} alt="concert ticket" />
+      <img src={robyn} alt="concert ticket" />
+    </main>
+  );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,41 +3,45 @@ import style from './Footer.module.css';
 
 export function Footer() {
   return (
-    <main className={style.footer}>
-      <div className={style.title}>
-        <h1>concerts nostalgia</h1>
-        <h2>my live concerts journey, revisited</h2>
+    <footer className={style.footer}>
+      <section className={style.footerInfo}>
+        <div className={style.title}>
+          <h1>concerts nostalgia</h1>
+          <h2>my live concerts journey, revisited</h2>
+        </div>
+        <div className={style.links}>
+          <a
+            href="https://www.linkedin.com/in/tulio-minini/"
+            target={'_blank'}
+            rel="noreferrer"
+            title="linkedin"
+          >
+            linkedIn
+            {/* <img src={linkIcon} alt="" /> */}
+          </a>
+          <a
+            href="https://github.com/t-minini"
+            target={'_blank'}
+            rel="noreferrer"
+            title="github"
+          >
+            github
+            {/* <img src={linkIcon} alt="" /> */}
+          </a>
+          <a
+            href="mailto:tulio.mminini@gmail.com"
+            target={'_blank'}
+            rel="noreferrer"
+            title="Send me an e-mail"
+          >
+            email
+            {/* <img src={linkIcon} alt="" /> */}
+          </a>
+        </div>
+      </section>
+      <div className={style.copyright}>
+        <p>&copy; 2023 Created, designed and built by Tulio Minini</p>
       </div>
-      <div className={style.links}>
-        <a
-          href="https://www.linkedin.com/in/tulio-minini/"
-          target={'_blank'}
-          rel="noreferrer"
-          title="linkedin"
-        >
-          linkedIn 
-          {/* <img src={linkIcon} alt="" /> */}
-        </a>
-        <a
-          href="https://github.com/t-minini"
-          target={'_blank'}
-          rel="noreferrer"
-          title="github"
-        >
-          github 
-          {/* <img src={linkIcon} alt="" /> */}
-        </a>
-        <a
-          href="mailto:tulio.mminini@gmail.com"
-          target={'_blank'}
-          rel="noreferrer"
-          title="Send me an e-mail"
-        >
-          email 
-          {/* <img src={linkIcon} alt="" /> */}
-        </a>
-      </div>
-      <p>&copy; 2023 Created, designed and built by Tulio Minini</p>
-    </main>
+    </footer>
   );
 }
