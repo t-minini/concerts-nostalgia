e816605458ee5b4934a860fddd37ca493f2fae00
Tulio <tulio.mminini@gmail.com>
2023-11-02 20:46:56 +0000

    :lipstick: react-scroll installed and added to the arrowdown at Hero component. small updated on the README

--- src/components/ConcertsList/ConcertsList.jsx
+++ src/components/ConcertsList/ConcertsList.jsx
@@ -1,11 +1,11 @@
 import style from './ConcertsList.module.css';
+import robyn from './../../assets/images/robyn-ticket.png';
 import halsey from './../../assets/images/halsey-ticket.png';
 import coldplay from './../../assets/images/coldplay-ticket.png';
-import robyn from './../../assets/images/robyn-ticket.png';
 
 export function ConcertsList() {
   return (
-    <main className={style.concertsList}>
+    <main id="tickets" className={style.concertsList}>
       <img src={halsey} alt="concert ticket" />
       <img src={coldplay} alt="concert ticket" />
       <img src={robyn} alt="concert ticket" />
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-scroll';
 import style from './Hero.module.css';
 import heroImg from './../../assets/images/kelvin-moquete-image.jpg';
 // import arrowDown from './../../assets/icons/arrow-down-wite.png';
@@ -9,11 +10,11 @@ export function Hero() {
         <h1>concerts nostalgia</h1>
         <h2>my live concerts journey, revisited</h2>
       </div>
-        <div className={style.arrowContainer}>
-          <div className={style.arrowDown}></div>
-          <div className={style.arrowDown}></div>
-          <div className={style.arrowDown}></div>
-        </div>
+      <Link to="tickets" smooth={true} className={style.arrowContainer}>
+        <div className={style.arrowDown}></div>
+        <div className={style.arrowDown}></div>
+        <div className={style.arrowDown}></div>
+      </Link>
       <div className={style.kelvinCopy}>
         photography by
         <a
@@ -28,7 +29,6 @@ export function Hero() {
         src={heroImg}
         alt="a crowd in a concert, during the night, with the main colors being orange and black"
       />
-
     </main>
   );
 }
