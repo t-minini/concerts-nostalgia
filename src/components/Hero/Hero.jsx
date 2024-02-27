2640a84446870286a7c92e7a2ce41921cc46698d
Tulio <tulio.mminini@gmail.com>
2024-02-27 11:22:58 +0000

    updating classes to use BEM Methodology

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,21 +5,26 @@ import heroImg from './../../assets/images/kelvin-moquete-image.jpg';
 export function Hero() {
   return (
     <section className={style.hero}>
-      <div className={style.text__container}>
-        <h1>concerts nostalgia</h1>
-        <p>my live concerts journey, revisited</p>
+      <div className={style.hero__container}>
+        <h1 className={style.hero__title}>concerts nostalgia</h1>
+        <p className={style.hero__text}>my live concerts journey, revisited</p>
       </div>
-      <Link to="tickets" smooth={true} className={style.arrow__container}>
-        <div className={style.arrowDown}></div>
-        <div className={style.arrowDown}></div>
-        <div className={style.arrowDown}></div>
+      <Link
+        to="tickets"
+        smooth={true}
+        className={style['hero__arrow-container']}
+      >
+        <div className={style['hero__arrow-down']}></div>
+        <div className={style['hero__arrow-down']}></div>
+        <div className={style['hero__arrow-down']}></div>
       </Link>
-      <div className={style.photo__copyright}>
-        photography by&nbsp;
+      <div className={style['hero__copyright-container']}>
+        <p className={style['hero__copyright-text']}>photography by&nbsp;</p>
         <a
           href="https://unsplash.com/@kelmoquete"
           target="_blank"
           rel="noreferrer"
+          className={style['hero__copyright-link']}
         >
           Kelvin Moquete
         </a>
@@ -27,6 +32,7 @@ export function Hero() {
       <img
         src={heroImg}
         alt="a crowd in a concert, during the night, with the main colors being orange and black"
+        className={style.hero__image}
       />
     </section>
   );
