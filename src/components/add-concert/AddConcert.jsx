aa196e20d0bea7a576b42c38f807f6410e3e5257
Tulio Minini <tulio.mminini@gmail.com>
2024-04-12 18:11:30 +0100

    add new modal design finished

--- src/components/add-concert/AddConcert.jsx
+++ src/components/add-concert/AddConcert.jsx
@@ -1,7 +1,18 @@
 // import style from './AddNewConcert.module.css';
 
 import React, { useState } from 'react';
-import { Row, Col, Flex, Modal, Form, Input, Select, Upload } from 'antd';
+import {
+  Row,
+  Col,
+  Flex,
+  Modal,
+  Form,
+  Input,
+  Select,
+  Upload,
+  DatePicker,
+  ConfigProvider,
+} from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import { api } from '../../api/concerts-nostalgia-api';
 import { useNavigate } from 'react-router-dom';
@@ -96,118 +107,193 @@ export function AddConcert() {
   return (
     <>
       <button onClick={showModal}>Add New</button>
-      <Modal
-        title="add new concert"
-        width="550px"
-        open={open}
-        onOk={handleSubmit}
-        confirmLoading={confirmLoading}
-        onCancel={handleCancel}
+      <ConfigProvider
+        theme={{
+          components: {
+            Modal: {
+              contentBg: '#212121',
+              headerBg: '#212121',
+              titleFontSize: '30px',
+              colorIcon: '#ffffff',
+              titleColor: '#ffffff',
+              colorIconHover: '#e34bf0',
+            },
+            Input: {
+              activeBorderColor: '#e34bf0',
+              hoverBorderColor: '#e34bf0',
+            },
+            Select: {
+              colorPrimaryHover: '#e34bf0',
+              colorPrimary: '#e34bf0',
+            },
+            DatePicker: {
+              colorPrimaryHover: '#e34bf0',
+              colorPrimary: '#e34bf0',
+            },
+            Button: {
+              colorPrimary: '#ffffff', // background Create Button
+              primaryColor: '#212121', // font color Create Button
+              colorPrimaryHover: '#e34bf0', //background hover Create Button
+              defaultHoverBg: '#e34bf0', // background hover Cancel Button
+              defaultColor: '#212121', // font color Cancel Button
+              defaultHoverColor: '#ffffff', // font color hover Cancel Button
+              defaultBorderColor: '#ffffff', // border Cancel Button
+            },
+          },
+        }}
       >
-        <Form
-          {...formItemLayout}
-          layout={formLayout}
-          form={form}
-          // initialValues={{
-          //   layout: formLayout,
-          // }}
-          onValuesChange={onFormLayoutChange}
-          // style={{
-          //   maxWidth: 600,
-          // }}
+        <Modal
+          title="add new concert"
+          width="550px"
+          open={open}
+          onOk={handleSubmit}
+          okText="Create"
+          confirmLoading={confirmLoading}
+          onCancel={handleCancel}
+          centered
         >
-          <Flex gap="middle" justify="space-between">
-            <Row
-              style={{
-                width: 240,
-              }}
-            >
-              <Col span={24}>
-                <Form.Item label="tour/concert">
-                  <Input placeholder="enter tour" value={concert.tour} />
-                </Form.Item>
-                <Form.Item label="location">
-                  <Input
-                    placeholder="enter location"
-                    value={concert.location}
-                  />
-                </Form.Item>
-                <Form.Item label="country">
-                  <Input placeholder="enter country" value={concert.country} />
-                </Form.Item>
-                <Form.Item label="rating">
-                  <Select placeholder="choose a rate" onChange={onThemeChange}>
-                    <Select.Option value={concert.rating[0]}>1</Select.Option>
-                    <Select.Option value={concert.rating[1]}>2</Select.Option>
-                    <Select.Option value={concert.rating[2]}>3</Select.Option>
-                    <Select.Option value={concert.rating[3]}>4</Select.Option>
-                    <Select.Option value={concert.rating[4]}>5</Select.Option>
-                  </Select>
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row
-              style={{
-                width: 240,
-              }}
-            >
-              <Col span={24}>
-                <Form.Item label="artist">
-                  <Input placeholder="enter artist" value={concert.artist} />
-                </Form.Item>
-                <Form.Item label="year">
-                  <Input placeholder="enter year" value={concert.year} />
-                </Form.Item>
-                <Form.Item label="city">
-                  <Input placeholder="enter city" value={concert.city} />
-                </Form.Item>
-                <Form.Item label="ticket style">
-                  <Select
-                    placeholder="choose a ticket style"
-                    onChange={onThemeChange}
+          <Form
+            {...formItemLayout}
+            layout={formLayout}
+            form={form}
+            // initialValues={{
+            //   layout: formLayout,
+            // }}
+            onValuesChange={onFormLayoutChange}
+            style={{
+              backgroundColor: '#212121',
+            }}
+          >
+            <Flex gap="middle" justify="space-between">
+              <Row
+                style={{
+                  width: 240,
+                }}
+              >
+                <Col span={24}>
+                  <Form.Item
+                    label={
+                      <label style={{ color: '#ffffff' }}>tour / concert</label>
+                    }
                   >
-                    <Select.Option value={concert.background[0]}>
-                      Style One
-                    </Select.Option>
-                    <Select.Option value={concert.background[1]}>
-                      Style Two
-                    </Select.Option>
-                    <Select.Option value={concert.background[2]}>
-                      Style Three
-                    </Select.Option>
-                    <Select.Option value={concert.background[3]}>
-                      Style Four
-                    </Select.Option>
-                    <Select.Option value={concert.background[4]}>
-                      Style Five
-                    </Select.Option>
-                  </Select>
-                </Form.Item>
-              </Col>
-            </Row>
-          </Flex>
-          <Form.Item label="images">
+                    <Input placeholder="enter tour" value={concert.tour} />
+                  </Form.Item>
+                  <Form.Item
+                    label={<label style={{ color: '#ffffff' }}>location</label>}
+                  >
+                    <Input
+                      placeholder="enter location"
+                      value={concert.location}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    label={<label style={{ color: '#ffffff' }}>country</label>}
+                  >
+                    <Input
+                      placeholder="enter country"
+                      value={concert.country}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    label={<label style={{ color: '#ffffff' }}>rating</label>}
+                  >
+                    <Select
+                      placeholder="choose a rate"
+                      onChange={onThemeChange}
+                    >
+                      <Select.Option value={concert.rating[0]}>1</Select.Option>
+                      <Select.Option value={concert.rating[1]}>2</Select.Option>
+                      <Select.Option value={concert.rating[2]}>3</Select.Option>
+                      <Select.Option value={concert.rating[3]}>4</Select.Option>
+                      <Select.Option value={concert.rating[4]}>5</Select.Option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row
+                style={{
+                  width: 240,
+                }}
+              >
+                <Col span={24}>
+                  <Form.Item
+                    label={<label style={{ color: '#ffffff' }}>artist</label>}
+                  >
+                    <Input placeholder="enter artist" value={concert.artist} />
+                  </Form.Item>
+                  <Form.Item
+                    label={<label style={{ color: '#ffffff' }}>year</label>}
+                  >
+                    <DatePicker
+                      placeholder="enter year"
+                      value={concert.year}
+                      picker="year"
+                      style={{
+                        width: 240,
+                      }}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    label={<label style={{ color: '#ffffff' }}>city</label>}
+                  >
+                    <Input placeholder="enter city" value={concert.city} />
+                  </Form.Item>
+                  <Form.Item
+                    label={
+                      <label style={{ color: '#ffffff' }}>ticket style</label>
+                    }
+                  >
+                    <Select
+                      placeholder="choose ticket style"
+                      onChange={onThemeChange}
+                    >
+                      <Select.Option value={concert.background[0]}>
+                        Style One
+                      </Select.Option>
+                      <Select.Option value={concert.background[1]}>
+                        Style Two
+                      </Select.Option>
+                      <Select.Option value={concert.background[2]}>
+                        Style Three
+                      </Select.Option>
+                      <Select.Option value={concert.background[3]}>
+                        Style Four
+                      </Select.Option>
+                      <Select.Option value={concert.background[4]}>
+                        Style Five
+                      </Select.Option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+              </Row>
+            </Flex>
             <Form.Item
-              name="dragger"
-              valuePropName="fileList"
-              // getValueFromEvent={normFile}
-              noStyle
+              label={<label style={{ color: '#ffffff' }}>images</label>}
             >
-              <Upload.Dragger name="files" action="/upload.do" disabled>
-                <p className="ant-upload-drag-icon">
-                  <UploadOutlined style={{ fontSize: '40px' }} />
-                </p>
-                <p className="ant-upload-text">
-                  Click or drag image to this area to upload
-                </p>
-                <p className="ant-upload-hint">
-                  Support for a single or bulk upload
-                </p>
-              </Upload.Dragger>
+              <Form.Item
+                name="dragger"
+                valuePropName="fileList"
+                // getValueFromEvent={normFile}
+                noStyle
+              >
+                <Upload.Dragger name="files" action="/upload.do" disabled>
+                  <p className="ant-upload-drag-icon">
+                    <UploadOutlined
+                      style={{ fontSize: '40px', color: '#ffffff' }}
+                    />
+                  </p>
+                  <p className="ant-upload-text" style={{ color: '#ffffff' }}>
+                    Click or drag image to this area to upload
+                  </p>
+                  <p className="ant-upload-hint" style={{ color: '#ffffff' }}>
+                    Support for a single or bulk upload
+                  </p>
+                </Upload.Dragger>
+              </Form.Item>
             </Form.Item>
-          </Form.Item>
-        </Form>
-      </Modal>
+          </Form>
+        </Modal>
+      </ConfigProvider>
     </>
   );
 }
